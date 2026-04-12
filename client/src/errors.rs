dda2627bd9d3bb324eaad2cebd5266b5cda3ccd2
Choncey Blaylock <rxdarkdawg10@gmail.com>
2026-04-12 16:11:10 -0400

    added opengl and hello triangle

--- client/src/errors.rs
+++ client/src/errors.rs
@@ -1,8 +1,15 @@
+use std::ffi::NulError;
+
+use sdl2::{IntegerOrSdlError, video::WindowBuildError};
+
 pub type Result<T> = core::result::Result<T, Error>;
 
 #[derive(Debug)]
 pub enum Error {
     StringError(String),
+    SDLErrorWindowBuild(WindowBuildError),
+    SDLCanvasError(IntegerOrSdlError),
+    GLNullError(NulError),
 }
 
 impl core::fmt::Display for Error {
@@ -11,9 +18,26 @@ impl core::fmt::Display for Error {
     }
 }
 
+impl From<NulError> for Error {
+    fn from(value: NulError) -> Self {
+        Self::GLNullError(value)
+    }
+}
+
 impl From<String> for Error {
     fn from(value: String) -> Self {
         Self::StringError(value)
+    }
+}
+
+impl From<WindowBuildError> for Error {
+    fn from(value: WindowBuildError) -> Self {
+        Self::SDLErrorWindowBuild(value)
+    }
+}
+impl From<IntegerOrSdlError> for Error {
+    fn from(value: IntegerOrSdlError) -> Self {
+        Self::SDLCanvasError(value)
     }
 }
 
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,9 +1,122 @@
+use std::ffi::{CStr, CString};
+
+use gl::UseProgram;
+use sdl2::{event::Event, keyboard::Keycode, pixels::Color, render::Canvas};
+
 use crate::errors::{Error, Result};
 
 mod errors;
+pub mod render_gl;
 
 fn main() -> Result<()> {
     let sdl2_context = sdl2::init()?;
+    let video_subsystem = sdl2_context.video()?;
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(4, 6);
+
+    let window = video_subsystem
+        .window("Game Dev", 800, 600)
+        .position_centered()
+        .borderless()
+        .fullscreen()
+        .opengl()
+        .build()?;
+
+    let gl_context = window.gl_create_context()?;
+    gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const _);
+
+    let mut sys_events = sdl2_context.event_pump()?;
+
+    unsafe {
+        gl::Viewport(0, 0, 800, 600);
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
+
+    let vert_shader =
+        render_gl::Shader::from_vert_source(&CString::new(include_str!("triangle.vert"))?)?;
+    let frag_shader =
+        render_gl::Shader::from_frag_source(&CString::new(include_str!("triangle.frag"))?)?;
+
+    let shader_program = render_gl::Program::from_shaders(&[vert_shader, frag_shader])?;
+    shader_program.set_used();
+
+    let vertices: Vec<f32> = vec![
+        -0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 1.0,
+    ];
+    let mut vbo: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenBuffers(1, &mut vbo);
+    }
+
+    unsafe {
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
+            vertices.as_ptr() as *const gl::types::GLvoid,
+            gl::STATIC_DRAW,
+        );
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+    }
+
+    let mut vao: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao);
+    }
+
+    unsafe {
+        gl::BindVertexArray(vao);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::EnableVertexAttribArray(0);
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            (6 * std::mem::size_of::<f32>()) as gl::types::GLint,
+            std::ptr::null(),
+        );
+
+        gl::EnableVertexAttribArray(1);
+        gl::VertexAttribPointer(
+            1,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            (6 * std::mem::size_of::<f32>()) as gl::types::GLint,
+            (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid,
+        );
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+    }
+
+    'running: loop {
+        for event in sys_events.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        shader_program.set_used();
+        unsafe {
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+        }
+
+        window.gl_swap_window();
+    }
 
     Ok(())
 }
