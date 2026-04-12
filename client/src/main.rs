4f64bf6654262817dbf314ca353d5694b89169f8
Choncey Blaylock <rxdarkdawg10@gmail.com>
2026-04-12 19:20:45 -0400

    a ton of refactor and segmentation

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,15 +1,20 @@
-use std::ffi::{CStr, CString};
+mod errors;
+mod render_gl;
+mod resources;
+
+use std::{
+    ffi::{CStr, CString},
+    path::Path,
+};
 
 use sdl2::{event::Event, keyboard::Keycode, pixels::Color, render::Canvas};
 
-use crate::errors::{Error, Result};
+use crate::resources::Resources;
 
-mod errors;
-pub mod render_gl;
-
-fn main() -> Result<()> {
-    let sdl2_context = sdl2::init()?;
-    let video_subsystem = sdl2_context.video()?;
+fn main() {
+    let res = Resources::from_relative_exe_path(Path::new("assets")).unwrap();
+    let sdl2_context = sdl2::init().unwrap();
+    let video_subsystem = sdl2_context.video().unwrap();
     let gl_attr = video_subsystem.gl_attr();
     gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
     gl_attr.set_context_version(4, 6);
@@ -20,26 +25,22 @@ fn main() -> Result<()> {
         .borderless()
         .fullscreen()
         .opengl()
-        .build()?;
+        .build()
+        .unwrap();
 
-    let gl_context = window.gl_create_context()?;
+    let gl_context = window.gl_create_context().unwrap();
     let gl = gl::Gl::load_with(|s| {
         video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void
     });
 
-    let mut sys_events = sdl2_context.event_pump()?;
+    let mut sys_events = sdl2_context.event_pump().unwrap();
 
     unsafe {
         gl.Viewport(0, 0, 800, 600);
         gl.ClearColor(0.3, 0.3, 0.5, 1.0);
     }
 
-    let vert_shader =
-        render_gl::Shader::from_vert_source(&gl, &CString::new(include_str!("triangle.vert"))?)?;
-    let frag_shader =
-        render_gl::Shader::from_frag_source(&gl, &CString::new(include_str!("triangle.frag"))?)?;
-
-    let shader_program = render_gl::Program::from_shaders(&gl, &[vert_shader, frag_shader])?;
+    let shader_program = render_gl::Program::from_res(&gl, &res, "shaders/triangle").unwrap();
     shader_program.set_used();
 
     let vertices: Vec<f32> = vec![
@@ -118,6 +119,4 @@ fn main() -> Result<()> {
 
         window.gl_swap_window();
     }
-
-    Ok(())
 }
--- client/src/render_gl.rs
+++ client/src/render_gl.rs
@@ -2,12 +2,47 @@ use gl;
 use std;
 use std::ffi::{CStr, CString};
 
+use crate::resources::{self, Resources};
+
+#[derive(Debug)]
+pub enum Error {
+    ResourceLoad {
+        name: String,
+        inner: resources::Error,
+    },
+    CannotDetermineShaderTypeForResource {
+        name: String,
+    },
+    CompileError {
+        name: String,
+        message: String,
+    },
+    LinkError {
+        name: String,
+        message: String,
+    },
+}
+
 pub struct Program {
     gl: gl::Gl,
     id: gl::types::GLuint,
 }
 
 impl Program {
+    pub fn from_res(gl: &gl::Gl, res: &Resources, name: &str) -> Result<Program, Error> {
+        const POSSIBLE_EXT: [&str; 2] = [".vert", ".frag"];
+
+        let shaders = POSSIBLE_EXT
+            .iter()
+            .map(|file_extension| Shader::from_res(gl, res, &format!("{}{}", name, file_extension)))
+            .collect::<Result<Vec<Shader>, Error>>()?;
+
+        Program::from_shaders(gl, &shaders[..]).map_err(|message| Error::LinkError {
+            name: name.into(),
+            message,
+        })
+    }
+
     pub fn from_shaders(gl: &gl::Gl, shaders: &[Shader]) -> Result<Program, String> {
         let program_id = unsafe { gl.CreateProgram() };
 
@@ -79,6 +114,27 @@ pub struct Shader {
 }
 
 impl Shader {
+    pub fn from_res(gl: &gl::Gl, res: &Resources, name: &str) -> Result<Shader, Error> {
+        const POSSIBLE_EXT: [(&str, gl::types::GLenum); 2] =
+            [(".vert", gl::VERTEX_SHADER), (".frag", gl::FRAGMENT_SHADER)];
+
+        let shader_kind = POSSIBLE_EXT
+            .iter()
+            .find(|&&(file_extension, _)| name.ends_with(file_extension))
+            .map(|&(_, kind)| kind)
+            .ok_or_else(|| Error::CannotDetermineShaderTypeForResource { name: name.into() })?;
+
+        let source = res.load_cstring(name).map_err(|e| Error::ResourceLoad {
+            name: name.into(),
+            inner: e,
+        })?;
+
+        Shader::from_source(gl, &source, shader_kind).map_err(|message| Error::CompileError {
+            name: name.into(),
+            message,
+        })
+    }
+
     fn from_source(gl: &gl::Gl, source: &CStr, kind: gl::types::GLenum) -> Result<Shader, String> {
         let id = shader_from_source(&gl, source, kind)?;
         Ok(Shader {
