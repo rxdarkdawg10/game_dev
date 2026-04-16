94a8b705ed8e1a19042c263d00c5ae79bf8dd8b5
Choncey Blaylock <rxdarkdawg10@gmail.com>
2026-04-16 08:10:22 -0400

    switched to SDL3 for 2d Rendering

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,122 +1,37 @@
-mod errors;
-mod render_gl;
-mod resources;
+use sdl3::event::Event;
+use sdl3::pixels::Color;
+use std::time::Duration;
 
-use std::{
-    ffi::{CStr, CString},
-    path::Path,
-};
+pub fn main() {
+    // Initialize SDL3 context
+    let sdl_context = sdl3::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
 
-use sdl2::{event::Event, keyboard::Keycode, pixels::Color, render::Canvas};
-
-use crate::resources::Resources;
-
-fn main() {
-    let res = Resources::from_relative_exe_path(Path::new("assets")).unwrap();
-    let sdl2_context = sdl2::init().unwrap();
-    let video_subsystem = sdl2_context.video().unwrap();
-    let gl_attr = video_subsystem.gl_attr();
-    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
-    gl_attr.set_context_version(4, 6);
-
+    // Build the window
     let window = video_subsystem
-        .window("Game Dev", 800, 600)
+        .window("SDL3 Rust Demo", 800, 600)
         .position_centered()
-        .borderless()
-        .fullscreen()
-        .opengl()
         .build()
         .unwrap();
 
-    let gl_context = window.gl_create_context().unwrap();
-    let gl = gl::Gl::load_with(|s| {
-        video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void
-    });
-
-    let mut sys_events = sdl2_context.event_pump().unwrap();
-
-    unsafe {
-        gl.Viewport(0, 0, 800, 600);
-        gl.ClearColor(0.3, 0.3, 0.5, 1.0);
-    }
-
-    let shader_program = render_gl::Program::from_res(&gl, &res, "shaders/triangle").unwrap();
-    shader_program.set_used();
-
-    let vertices: Vec<f32> = vec![
-        -0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 1.0,
-    ];
-    let mut vbo: gl::types::GLuint = 0;
-    unsafe {
-        gl.GenBuffers(1, &mut vbo);
-    }
-
-    unsafe {
-        gl.BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl.BufferData(
-            gl::ARRAY_BUFFER,
-            (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr,
-            vertices.as_ptr() as *const gl::types::GLvoid,
-            gl::STATIC_DRAW,
-        );
-        gl.BindBuffer(gl::ARRAY_BUFFER, 0);
-    }
-
-    let mut vao: gl::types::GLuint = 0;
-    unsafe {
-        gl.GenVertexArrays(1, &mut vao);
-    }
-
-    unsafe {
-        gl.BindVertexArray(vao);
-
-        gl.BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl.EnableVertexAttribArray(0);
-        gl.VertexAttribPointer(
-            0,
-            3,
-            gl::FLOAT,
-            gl::FALSE,
-            (6 * std::mem::size_of::<f32>()) as gl::types::GLint,
-            std::ptr::null(),
-        );
-
-        gl.EnableVertexAttribArray(1);
-        gl.VertexAttribPointer(
-            1,
-            3,
-            gl::FLOAT,
-            gl::FALSE,
-            (6 * std::mem::size_of::<f32>()) as gl::types::GLint,
-            (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid,
-        );
-
-        gl.BindBuffer(gl::ARRAY_BUFFER, 0);
-        gl.BindVertexArray(0);
-    }
+    // Initialize Canvas for drawing
+    let mut canvas = window.into_canvas();
+    let mut event_pump = sdl_context.event_pump().unwrap();
 
     'running: loop {
-        for event in sys_events.poll_iter() {
+        // Handle events
+        for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
+                Event::Quit { .. } => break 'running,
                 _ => {}
             }
         }
 
-        unsafe {
-            gl.Clear(gl::COLOR_BUFFER_BIT);
-        }
+        // Update & Render
+        canvas.set_draw_color(Color::RGB(0, 64, 255));
+        canvas.clear();
+        canvas.present();
 
-        shader_program.set_used();
-        unsafe {
-            gl.BindVertexArray(vao);
-            gl.DrawArrays(gl::TRIANGLES, 0, 3);
-        }
-
-        window.gl_swap_window();
+        std::thread::sleep(Duration::from_millis(16)); // Cap at ~60 FPS
     }
 }
